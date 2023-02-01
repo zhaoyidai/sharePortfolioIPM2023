4a683d6698fa3cc6246294394deb16038117a40d
zdai2 <zdai2923@gmail.com>
2023-02-01 15:56:18 +0100

    US6 Zhaoyi&Zhongwei

--- src/test/java/tp04/metier/ActionSimpleTest.java
+++ src/test/java/tp04/metier/ActionSimpleTest.java
@@ -15,16 +15,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ActionSimpleTest {
     
     private static final String EXPECTED_LIB="Toto";
+    private static final String ExPECTED_AC_LIB="Tata";
     
     public ActionSimpleTest() {
     }
 
     @Test
     public void testgetLibShouldPass() {
-        final ActionSimple as1=new ActionSimple("Toto");
+        final ActionSimple as1=new ActionSimple(EXPECTED_LIB);
         final String result=as1.getLibelle();
         
         Assertions.assertSame(EXPECTED_LIB, result, "Should be the same");
+    }
+    
+    @Test
+    public void testSetActionComposee(){
+        final ActionSimple as1=new ActionSimple("Toto");
+        final ActionComposee acc=new ActionComposee("Tata");
+        as1.setActionComp(acc);
+        final String result=as1.getActionComp().getLibelle();
+        
+        Assertions.assertSame(ExPECTED_AC_LIB, result, "Should be the same");
     }
     
 }
