4eeb51ed5acb2ede2a4110dad6343bfe6a507b25
wailbenmansour <wailbenmansour@dhcp-12-89-60.ut-capitole.fr>
2023-02-01 16:57:05 +0100

    commit Test Action simple

--- src/test/java/tp04/metier/ActionSimpleTest.java
+++ src/test/java/tp04/metier/ActionSimpleTest.java
@@ -4,6 +4,8 @@
  */
 package tp04.metier;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +17,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ActionSimpleTest {
     
     private static final String EXPECTED_LIB="Toto";
+    private static final float EXPECTED_VALUE=0F;
+    private static final Map<Jour, Cours> EXPECTED_MAP = new HashMap<>();
+    
     
     public ActionSimpleTest() {
     }
@@ -25,6 +30,57 @@ public class ActionSimpleTest {
         final String result=as1.getLibelle();
         
         Assertions.assertSame(EXPECTED_LIB, result, "Should be the same");
+    }
+    
+     @Test
+    public void testEqualsJourDonneActionSimple() {
+        //Attributs à tester
+        final Jour j1 = new Jour(2022,20);
+        final float v1 = 0F;
+        
+        //Instanciation d'une action simple
+        final ActionSimple as1 = new ActionSimple("France 2");
+        //Enregistrer le cours v1 d'une action simple à un jour j1
+        as1.enrgCours(j1, v1);
+        
+        //Obtenir le cours à partir d'un jour
+        final float valueObtenue = as1.valeur(j1);
+        
+        //Tester si le cours d'un jour est le même que le cours attendu
+        Assertions.assertEquals(EXPECTED_VALUE, valueObtenue, "Le cour est le même à ce jour!");
+    }
+    
+    @Test
+    public void testNotEqualsJourDonneActionSimple() {
+        //Attributs à tester
+        final Jour j1 = new Jour(2022,20);
+        final float v1 = 1F;
+        
+        //Instanciation d'une action simple
+        final ActionSimple as1 = new ActionSimple("France 2");
+        //Enregistrer le cours v1 d'une action simple à un jour j1
+        as1.enrgCours(j1, v1);
+        
+        //Obtenir le cours à partir d'un jour
+        final float valeurObtenue = as1.valeur(j1);
+        
+        //Tester si le cours d'un jour n'est pas le même que le cours attendu
+        Assertions.assertNotEquals(EXPECTED_VALUE, valeurObtenue, "Le cour n'est pas le même à ce jour!");
+    }
+    
+    @Test
+    public void testEqualsEnregistrerCoursActionSimple() {
+        //Attributs à tester
+        final Jour j1 = new Jour(2022,20);
+        final float v1 = 0F;
+        
+        //Instanciation d'une action simple
+        final ActionSimple as1 = new ActionSimple("France 2");
+        //Enregistrer le cours v1 d'une action simple à un jour j1
+        as1.enrgCours(j1, v1);
+      
+        //Tester si le cours d'un jour est le même que le cours attendu
+        Assertions.assertEquals(EXPECTED_MAP, valueObtenue, "Le cour est le même à ce jour!");
     }
     
 }
