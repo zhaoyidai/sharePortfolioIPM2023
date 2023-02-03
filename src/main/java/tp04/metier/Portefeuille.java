cf2b3456c24135fdf1964b09c0effb6a9866ed91
wailbenmansour <wailbenmansour@dhcp-12-89-60.ut-capitole.fr>
2023-02-03 12:29:16 +0100

    Merge origin/master Wail & Ali : Correction (sysout by logger)
    Conflicts:
    	src/test/java/tp04/metier/PortefeuilleTest.java
    	Run.java

--- src/main/java/tp04/metier/Portefeuille.java
+++ src/main/java/tp04/metier/Portefeuille.java
@@ -7,8 +7,6 @@ package tp04.metier;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
-
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
