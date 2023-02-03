9678c6cfe1b3405020ae255005864d012345e869
wailbenmansour <wailbenmansour@dhcp-12-89-60.ut-capitole.fr>
2023-02-03 12:59:35 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/tp04/metier/Portefeuille.java

--- src/main/java/tp04/exec/Run.java
+++ src/main/java/tp04/exec/Run.java
@@ -41,37 +41,37 @@ public class Run {
         bnp.enrgCours(j1, 100);
         bnp.enrgCours(j2, 200);
         // affichage des cours - comme 1 action simple et 1 action
-        LOG.log(Level.INFO, "Action simple *bnp* à j1 : " + bnp.valeur(j1));
-        LOG.log(Level.INFO, "Action *Banque-Assurance* à j2 : " + bqAss.valeur(j2));
+        LOG.log(Level.INFO, "Action simple *bnp* à j1 : ", bnp.valeur(j1));
+        LOG.log(Level.INFO, "Action *Banque-Assurance* à j2 : ",  bqAss.valeur(j2));
 
         Portefeuille p;
         p = new Portefeuille();
         p.acheter(axa, 10);
-        LOG.log(Level.INFO, "Portefeuille : " + p);
+        LOG.log(Level.INFO, "Portefeuille : {0}", p);
         p.acheter(bnp, 20);
-        LOG.log(Level.INFO, "Portefeuille : " + p);
+        LOG.log(Level.INFO, "Portefeuille : {0}",  p);
         p.acheter(bqAss, 5);
-        LOG.log(Level.INFO, "Portefeuille : " + p);
+        LOG.log(Level.INFO, "Portefeuille : {0}", p);
         p.acheter(bqAss, 15);
-        LOG.log(Level.INFO, "Portefeuille : " + p);
-        LOG.log(Level.INFO, "Portefeuille à j1 : " + p.valeur(j1));
+        LOG.log(Level.INFO, "Portefeuille : {0}", p);
+        LOG.log(Level.INFO, "Portefeuille à j1 : {0}", p.valeur(j1));
         p.vendre(axa, 5);
-        LOG.log(Level.INFO, "Portefeuille : " + p);
+        LOG.log(Level.INFO, "Portefeuille : {0}", p);
         p.vendre(axa, 5);
-        LOG.log(Level.INFO, "Portefeuille : " + p);
+        LOG.log(Level.INFO, "Portefeuille : {0}", p);
         p.vendre(axa, 5);
-        LOG.log(Level.INFO, "Portefeuille : " + p);
+        LOG.log(Level.INFO, "Portefeuille : {0}", p);
         p.vendre(bnp, 50);
-        LOG.log(Level.INFO, "Portefeuille : " + p);
+        LOG.log(Level.INFO, "Portefeuille : {0}", p);
 
         
         LOG.log(Level.INFO, "Liste acheter : ");
         for(Action a:p.getListeAchete()){
-            LOG.log(Level.INFO, a.getLibelle());
+            LOG.log(Level.INFO, "message {0}", a.getLibelle());
         }
 
  
-        LOG.log(Level.INFO, p.toStringDetail());
+        LOG.log(Level.INFO, "message : {0}", p.toStringDetail());
 
     }
 
